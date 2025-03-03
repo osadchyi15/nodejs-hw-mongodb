7e38e6365fb78f73b04c26bf5029b76b973cacae
Ievgen Osadchyi <osadchyi15@gmail.com>
2025-03-04 00:09:37 +0100

    init

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
-import express from 'express';
+import express, { Router } from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import contactsRouter from './routers/contacts.js';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFounfHandler.js';
 
 const PORT = Number(getEnvVar('PORT', 3000));
 
@@ -10,7 +12,12 @@ export const setupServer = () => {
   const app = express();
 
   app.use(cors());
-  app.use(express.json());
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+      limit: '100kb',
+    }),
+  );
 
   app.use(
     pino({
@@ -20,45 +27,11 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
+  app.use(contactsRouter);
 
-    res.json({
-      status: '200',
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
+  app.use('*', notFoundHandler);
 
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      res.status(404).json({
-        message: `Contact with id ${contactId} not found`,
-      });
-      return;
-    }
-
-    res.status(200).json({
-      status: '200',
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
