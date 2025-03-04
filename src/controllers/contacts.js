eea7c3fad7dabcb5f55450523b9103b3e990c200
Ievgen Osadchyi <osadchyi15@gmail.com>
2025-03-04 19:29:01 +0100

    trow-fixed

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -48,8 +48,7 @@ export const deleteContactController = async (req, res, next) => {
   const contact = await deleteContact(contactId);
 
   if (!contact) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.status(204).send();
@@ -61,8 +60,7 @@ export const upsertContactController = async (req, res, next) => {
   const result = await updateContact(contactId, req.body, { upsert: true });
 
   if (!result) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
 
   const status = result.isNew ? 201 : 200;
@@ -79,8 +77,7 @@ export const patchContactController = async (req, res, next) => {
   const result = await updateContact(contactId, req.body);
 
   if (!result) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.json({
