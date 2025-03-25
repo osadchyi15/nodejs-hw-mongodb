0032c2ee0634b7f2bdaa5123cab333e7f2b8a7a9
Ievgen Osadchyi <osadchyi15@gmail.com>
2025-03-25 23:03:54 +0100

    init

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -13,6 +13,7 @@ cloudinary.v2.config({
 
 export const saveFileToCloudinary = async (file) => {
   const response = await cloudinary.v2.uploader.upload(file.path);
+
   await fs.unlink(file.path);
   return response.secure_url;
 };
